521b3556d3baa0c3bd7553bb3698fea2f6dc53b4
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-04-15 17:20:08 +0200

    Back to development: 1.0.0rc2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.0rc1'
+version = '1.0.0rc2.dev0'
 
 long_description = (
     read('README.md')
