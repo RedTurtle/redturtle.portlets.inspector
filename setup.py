564db8a7d6e9b64592ca634295f38ddcda4ee9fd
Alessandro Pisa <alessandro.pisa@redturtle.it>
2013-04-15 17:19:31 +0200

    Preparing release 1.0.0rc1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.9.0.dev0'
+version = '1.0.0rc1'
 
 long_description = (
     read('README.md')
